b628062289d4193b35f752a072ddd8630919e6fb
Bimesh De Silva <bimeshde@gmail.com>
2018-09-05 19:00:30 -0400

    Added visual feedback for request success / failure

--- Backend/main.go
+++ Backend/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"log"
 )
 
@@ -12,20 +12,31 @@ type myReturn struct {
 	Response string `json:"response"`
 }
 
+type RequestBody struct {
+	Name string `json:"name"`
+}
+
 func handle(ctx context.Context, name events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Print("Called by ", name)
 	log.Print("context ", ctx)
 	headers := map[string]string{"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"}
 
+	var body RequestBody
+	jsonParseError := json.Unmarshal([]byte(name.Body), &body)
+	if jsonParseError != nil {
+		log.Println(jsonParseError)
+		return events.APIGatewayProxyResponse{500, headers, "Internal Server Error", false}, nil
+	}
+
 	code := 200
-	response, error := json.Marshal(myReturn{Response:"Hello, " + name.Body})
-	if error != nil {
-		log.Println(error)
+	response, jsonBuildError := json.Marshal(myReturn{Response: "Hello, " + body.Name})
+	if jsonBuildError != nil {
+		log.Println(jsonBuildError)
 		response = []byte("Internal Server Error")
 		code = 500
 	}
 
-	return events.APIGatewayProxyResponse {code, headers, string(response), false}, nil
+	return events.APIGatewayProxyResponse{code, headers, string(response), false}, nil
 }
 
 func main() {
--- Web/js/main.js
+++ Web/js/main.js
@@ -1,15 +1,30 @@
 const BASE_URL = "https://iiaeky1f01.execute-api.us-east-1.amazonaws.com/prod/";
 const TRIGGER_URL = "lambdatrigger";
 
+function getValue() {
+	return document.getElementById("bimde_text_input").value;
+}
+
+function setTextWithValue(s) {
+	document.getElementById("bimde_text_field").innerHTML = s;
+}
+
 function toGW() {
+	var value = getValue();
 	fetch(BASE_URL + TRIGGER_URL, {
 		method: 'POST',
-		body: '{"name":"Bimesh"}',
+		body: JSON.stringify({name: value}),
 		headers:{
 			'Content-Type': 'application/json'
 		}
 	})
 	.then(data => data.json())
-	.then(json => console.log(json))
-	.catch(error => console.log(error));
+	.then(json => {
+		console.log(json);
+		setTextWithValue("The AWS Lambda function says, \"" + json.response + "\"");
+	})
+	.catch(error => {
+		console.log(error);
+		setTextWithValue("Whoops, something went wrong :(<br> Check out the console if you want more details.");
+	});
 }
